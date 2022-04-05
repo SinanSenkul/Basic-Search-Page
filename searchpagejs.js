4959711f05230846143e38187fb9c8bb12d5e369
Sinan Şenkul <101463814+SinanSenkul@users.noreply.github.com>
2022-04-05 17:49:42 +0300

    Update searchpagejs.js

--- searchpagejs.js
+++ searchpagejs.js
@@ -1,46 +1,3 @@
-function googleOpen(){
-        tB=document.getElementById("googleBox").value;
-        address="https://www.google.com/search?q="+tB+"";
-        if(tB != ""){
-            window.open(address);
-            document.getElementById("googleBox").value="";
-            return;
-        }
-}
-
-function youtubeOpen(){
-    tB=document.getElementById("youtubeBox").value;
-    address="https://youtube.com/results?search_query="+tB+"";
-    if(tB != ""){
-        window.open(address);
-        document.getElementById("youtubeBox").value="";
-        return;
-    }
-}
-
-function radioCheckOpen(){
-       if(document.getElementById("googleRadio").checked === true){
-        window.open("https://www.google.com");
-        document.getElementById("googleRadio").checked=false;
-        return;
-       }
-       if(document.getElementById("youtubeRadio").checked === true){
-        window.open("https://www.youtube.com");
-        document.getElementById("youtubeRadio").checked=false;
-        document.getElementById("button3").value="➥";
-        return;
-       }
-       if(document.getElementById("wikiRadio").checked === true){
-        window.open("https://www.wikipedia.org");
-        document.getElementById("wikiRadio").checked=false;
-        document.getElementById("button3").value="➥";
-       }
-}
-
-var r=0;
-var g=0;
-var b=0;
-
 function colorChanger(){
         if(r===255 && g===255 && b===255){
             r=r-1;
@@ -113,14 +70,8 @@ function pageColor(){
         document.getElementById("sec1").style="background-color: #264653";
         document.getElementById("sec2").style="background-color: #264653";
         document.getElementById("sec3").style="background-color: #264653";
-        document.getElementById("p1").style="color: white";
-        document.getElementById("p2").style="color: white";
-        document.getElementById("p3").style="color: white";
-        document.getElementById("l1").style="color: white";
-        document.getElementById("l2").style="color: white";
-        document.getElementById("l3").style="color: white";
-        document.getElementById("button3").style="color: white";
         document.getElementById("nightButton").value="💡";
+        document.getElementById("nightButton").title="Day Mode";
         return;
     }
     if(document.getElementById("nightButton").value==="💡"){
@@ -128,13 +79,7 @@ function pageColor(){
         document.getElementById("sec1").style="background-color: lightgrey";
         document.getElementById("sec2").style="background-color: lightgrey";
         document.getElementById("sec3").style="background-color: lightgrey";
-        document.getElementById("p1").style="color: black";
-        document.getElementById("p2").style="color: black";
-        document.getElementById("p3").style="color: black";
-        document.getElementById("l1").style="color: black";
-        document.getElementById("l2").style="color: black";
-        document.getElementById("l3").style="color: black";
-        document.getElementById("button3").style="color: black";
         document.getElementById("nightButton").value="🌙";
+        document.getElementById("nightButton").title="Night Mode";
     }
 }
